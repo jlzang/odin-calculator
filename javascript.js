ac317b3655307aabbe90a5ff23d3bc7e283cf621
jlzang <jlzang99@gmail.com>
2024-01-19 18:53:24 -0600

    Create functions for basic math operators

---
+++ javascript.js
@@ -0,0 +1,19 @@
+const add = function(num1, num2) {
+    return num1 + num2;
+};
+console.log(add(1,2));
+
+const subtract = function(num1, num2) {
+    return num1 - num2;
+};
+console.log(subtract(5,3));
+
+const multiply = function(num1, num2) {
+    return num1 * num2;
+};
+console.log(multiply(3,6));
+
+const divide = function(num1, num2) {
+    return num1 / num2;
+};
+console.log(divide(27,3));
