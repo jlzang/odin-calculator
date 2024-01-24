bd324c70626127c88257220008b446d620fcdde4
jlzang <jlzang99@gmail.com>
2024-01-24 15:30:53 -0600

    Create operate() function
    Takes two numbers then calls one of the operator functions on the two
    numbers

--- javascript.js
+++ javascript.js
@@ -1,19 +1,31 @@
-const add = function(num1, num2) {
-    return num1 + num2;
+const add = function(n1, n2) {
+    return n1 + n2;
 };
-console.log(add(1,2));
 
-const subtract = function(num1, num2) {
-    return num1 - num2;
+const subtract = function(n1, n2) {
+    return n1 - n2;
 };
-console.log(subtract(5,3));
 
-const multiply = function(num1, num2) {
-    return num1 * num2;
+const multiply = function(n1, n2) {
+    return n1 * n2;
 };
-console.log(multiply(3,6));
 
-const divide = function(num1, num2) {
-    return num1 / num2;
+const divide = function(n1, n2) {
+    return n1 / n2;
 };
-console.log(divide(27,3));
+
+let num1;
+let num2;
+let operator;
+
+const operate = function(num1, num2, operator) {
+    if (operator === "+") {
+        return add(num1, num2);
+    } else if (operator === "-") {
+        return subtract(num1, num2);
+    } else if (operator === "*") {
+        return multiply(num1, num2);
+    } else if (operator === "/") {
+        return divide(num1, num2);
+    };
+};
