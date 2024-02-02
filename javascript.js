3c6a31acc824db717ea00f5b70bdf7bf50bbbfdb
jlzang <jlzang99@gmail.com>
2024-02-01 19:15:54 -0600

    Scrap everything and start again
    Create functions that update the display and store the selected
    operator in a variable

--- javascript.js
+++ javascript.js
@@ -1,24 +1,25 @@
-const add = function(n1, n2) {
+const add = function (n1, n2) {
     return n1 + n2;
 };
 
-const subtract = function(n1, n2) {
+const subtract = function (n1, n2) {
     return n1 - n2;
 };
 
-const multiply = function(n1, n2) {
+const multiply = function (n1, n2) {
     return n1 * n2;
 };
 
-const divide = function(n1, n2) {
+const divide = function (n1, n2) {
     return n1 / n2;
 };
 
-let num1;
-let num2;
-let operator;
+let num1 = null;
+let num2 = null;
+let operator = null;
+let currentDisplay = null;
 
-const operate = function(num1, num2, operator) {
+const operate = function (num1, num2, operator) {
     if (operator === "+") {
         return add(num1, num2);
     } else if (operator === "-") {
@@ -30,11 +31,52 @@ const operate = function(num1, num2, operator) {
     };
 };
 
-let buttons = document.querySelectorAll("button");
-let display = document.querySelector(".display")
+let numBtns = document.querySelectorAll(".number");
+let opBtns = document.querySelectorAll(".operator");
+let equalsBtn = document.querySelector(".equals");
+let clearBtn = document.querySelector(".clear");
+let display = document.querySelector(".display");
 
-buttons.forEach((item) => {
+numBtns.forEach((item) => {
     item.addEventListener("click", (event) => {
-        display.textContent += `${event.target.id}`;
+        updateDisplay(event);
+    });
+});
+
+opBtns.forEach((item) => {
+    item.addEventListener("click", (event) => {
+        getOperator(event);
     })
 })
+function updateDisplay(event) {
+    display.textContent += `${event.target.id}`;
+    currentDisplay = display.textContent;
+}
+
+function getOperator(event) {
+    operator = event.target.id;
+}
+
+
+/*let numBtns = document.querySelectorAll(".number");
+let display = document.querySelector(".display");
+let displayValue = "empty";
+
+numBtns.forEach((item) => {
+    item.addEventListener("click", (event) => {
+        display.textContent += `${event.target.id}`;
+        displayValue = display.textContent;
+    });
+});
+
+let opBtns = document.querySelectorAll(".operator");
+
+opBtns.forEach(item => {
+    item.addEventListener("click", (event) => {
+        if (displayValue === "empty") {
+            return;
+        };
+        return operator = `${event.target.id}`;
+    })
+})
+*/
